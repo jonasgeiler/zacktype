1c1e5ee57e56ec9368d865914254d6b7e2e8fc35
Skayo <jonas@skayo.dev>
2021-11-02 16:20:24 +0100

    Added very very basic front-end to test the current state of the game

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,44 @@
-<h1>zacktype</h1>
+<script lang="ts">
+	import TypingGame from '$lib/core/TypingGame';
+
+	const typingGame = new TypingGame();
+	const {
+		text,
+		characterStates,
+		cursorPosition,
+		wpm,
+		cps,
+		accuracy,
+	} = typingGame.getStores();
+</script>
+
+<div>
+	{#each $text as character, index (character + index)}
+		<span class:active={$cursorPosition === index}
+		      class:correct={$characterStates[index] === TypingGame.CharacterState.Correct}
+		      class:incorrect={$characterStates[index] === TypingGame.CharacterState.Incorrect}
+		>{character}</span>
+	{/each}
+</div>
+
+<input type="text" on:keydown|preventDefault={e => typingGame.handleKey(e.key)} />
+
+<p>
+	WPM: {$wpm}<br>
+	CPS: {$cps}<br>
+	Accuracy: {$accuracy}%<br>
+</p>
+
+<style>
+	span.active {
+		background-color: #aaa;
+	}
+
+	span.correct {
+		color: #0f0;
+	}
+
+	span.incorrect {
+		color: #f00;
+	}
+</style>
