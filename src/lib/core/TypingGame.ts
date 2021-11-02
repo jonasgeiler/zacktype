98c55d758d5ef039bee8c83787baf45a87dd08ef
Skayo <jonas@skayo.dev>
2021-11-02 10:07:15 +0100

    Introduced CharacterState and Character types

--- src/lib/core/TypingGame.ts
+++ src/lib/core/TypingGame.ts
@@ -1,6 +1,18 @@
+enum CharacterState {
+	Unreached,
+	Correct,
+	Incorrect
+}
+
+interface Character {
+	char: string;
+	state: CharacterState;
+}
 
 class TypingGame {
-	
+
+
+
 }
 
 export default TypingGame;
